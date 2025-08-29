2604a842d57e73624bd3356ac47ab914a328cba2
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-08-29 10:52:42 +0300

    fix:remove summary_path

--- tuning/bayes_tuning.py
+++ tuning/bayes_tuning.py
@@ -54,11 +54,9 @@ def run_tuning(n_trials=3, study_name="bayesian_tuning", local_rank=0, device=No
 
    
     if local_rank == 0:
-        summary_path,trials_path = initialize_logs(study_name)
-    else:
-        summary_path = f"tuning/{study_name}_summary.txt"
-    logger.info(f"[Rank {local_rank}] Starting Bayesian tuning with {n_trials} trials")
-    logger.info(f"[Rank {local_rank}] Trials Log: {trials_path}")
+        trials_path = initialize_logs(study_name)
+        logger.info(f"[Rank {local_rank}] Starting Bayesian tuning with {n_trials} trials")
+        logger.info(f"[Rank {local_rank}] Trials Log: {trials_path}")
 
     try:
         study.optimize(lambda trial: objective(trial, device, flash), n_trials=n_trials, show_progress_bar=(local_rank == 0))
