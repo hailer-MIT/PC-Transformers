30f2ad6b7d4f8e1f6bf95db975532ae8b22424d7
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-08-29 15:49:43 +0300

    remove summary path

--- tuning/bayes_tuning.py
+++ tuning/bayes_tuning.py
@@ -59,26 +59,37 @@ def run_tuning(n_trials=30, study_name="bayesian_tuning", local_rank=0, device=N
     # logger.info(f"[Rank {local_rank}] Starting Bayesian tuning with {n_trials} trials")
     # logger.info(f"[Rank {local_rank}] Trials Log: {trials_path}")
     if local_rank == 0:
-        summary_path, trials_path = initialize_logs(study_name)
+        trials_path = initialize_logs(study_name)
     else:
-        summary_path = f"tuning/{study_name}_summary.txt"
         trials_path = f"tuning/{study_name}_trials.txt"
         
     logger.info(f"[Rank {local_rank}] Starting Bayesian tuning with {n_trials} trials")
-    logger.info(f"[Rank {local_rank}] Summary Log: {summary_path}")
     logger.info(f"[Rank {local_rank}] Trials Log: {trials_path}")
-
-
+    study.optimize(lambda trial: objective(trial, device, flash), n_trials=n_trials, show_progress_bar= True)
     try:
-        study.optimize(lambda trial: objective(trial, device, flash), n_trials=n_trials, show_progress_bar=(local_rank == 0))
-        logger.info(f"[Rank {local_rank}] Bayesian tuning completed!")
-    
-        if local_rank == 0 and study.best_trial:
+        if local_rank == 0:
+            logger.info(f"[Rank {local_rank}] Tuning completed.")
+            
+            if len(study.trials) > 0:
                 trial = study.best_trial
                 logger.info(f"Best trial: {trial.number}. Best value: {trial.value:.5f}")
                 write_final_results(f"tuning/{study_name}_results.txt", trial)
+            else:
+                logger.warning("[Rank 0] No completed trials to report.")
+            
         dist.barrier(device_ids=[local_rank])   
         return study
+
+    # try:
+    #     study.optimize(lambda trial: objective(trial, device, flash), n_trials=n_trials, show_progress_bar=(local_rank == 0))
+    #     logger.info(f"[Rank {local_rank}] Bayesian tuning completed!")
+    
+        # if local_rank == 0 and study.best_trial:
+        #         trial = study.best_trial
+        #         logger.info(f"Best trial: {trial.number}. Best value: {trial.value:.5f}")
+        #         write_final_results(f"tuning/{study_name}_results.txt", trial)
+        # dist.barrier(device_ids=[local_rank])   
+        # return study
     
     except KeyboardInterrupt:
         logger.warning(f"[Rank {local_rank}] Tuning interrupted")
