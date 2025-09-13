c4a1fbc0de5080c51284fd95a2823623258fca4c
Saad Musema <saadmusema3@gmail.com>
2025-09-13 14:28:37 +0300

    fix: update energy function to pc_e and attn_weights to float32

--- tuning/config.py
+++ tuning/config.py
@@ -37,8 +37,8 @@ def get_dynamic_model_config(trial, vocab_size, flash=False):
         num_epochs=3,
         update_bias=update_bias,
         use_lateral=True,
-        internal_energy_fn_name="mse",
-        output_energy_fn_name="kld",
+        internal_energy_fn_name="pc_e",
+        output_energy_fn_name="pc_e",
         use_flash_attention=flash
     )
 
